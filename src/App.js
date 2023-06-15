5b54a29f9ac40394b2bde6b966d77e7e8fdb261b
Shubham Shelar <shubhamshelar1211@gmail.com>
2023-06-15 09:56:00 +0530

    button click

--- src/App.js
+++ src/App.js
@@ -9,22 +9,15 @@ function App() {
 
 // UserDefineTag
 function CounterApp() {
-  // Data Member :: Optional
-  let counter = 1;
-  let id = 100;
-  let pi = 3.14;
-  let str = "Hello World";
-  let active = true;
+  // Member Function
+  let likeMeAction = () => {
+    // alert();
+    console.log("I m button click");
+  };
 
-  // Member function :: Optional
-  let likeMeAction = () => {};
-  let dislikeMeAction = () => {};
-
-  // JSX :: VIEW :: USER-INTERFACE :: Required
   return (
     <>
-      <h1>{counter}</h1>
-      <input type="button" value="Like Me" />
+      <input type="button" value="Like Me" onClick={likeMeAction} />
     </>
   );
 }
