415d8dd8c6f2e185305a25ef932285600214daca
Shubham Shelar <shubhamshelar1211@gmail.com>
2023-06-15 09:04:13 +0530

    multiple functions

--- src/App.js
+++ src/App.js
@@ -1,7 +1,37 @@
+// User Defined Tag: <App />
 function App() {
   return (
     <>
-      <h1>Hello World</h1>
+      <AppHeader />
+      <AppBody />
+      <AppFooter />
+    </>
+  );
+}
+
+// User Defined Tag: <AppHeader />
+function AppHeader() {
+  return (
+    <>
+      <h1>Header</h1>
+    </>
+  );
+}
+
+// User Defined Tag: <AppBody />
+function AppBody() {
+  return (
+    <>
+      <h1>App Body</h1>
+    </>
+  );
+}
+
+// User Defined Tag: <AppFooter />
+function AppFooter() {
+  return (
+    <>
+      <h1>App Footer</h1>
     </>
   );
 }
