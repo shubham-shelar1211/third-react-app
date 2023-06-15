badefebc7ba4e0e445b1043a14bd0086c13b94ff
Shubham Shelar <shubhamshelar1211@gmail.com>
2023-06-15 09:51:35 +0530

    function structure

--- src/App.js
+++ src/App.js
@@ -1,15 +1,30 @@
-import AppBody from "./pages/AppBody";
-import AppFooter from "./pages/AppFooter";
-import AppHeader from "./pages/AppHeader";
-
-// User Defined Tag: <App />
 function App() {
   return (
     <>
-      <AppHeader />
-      <AppBody />
-      <AppBody />
-      <AppFooter />
+      <h1>Counter App</h1>
+      <CounterApp />
+    </>
+  );
+}
+
+// UserDefineTag
+function CounterApp() {
+  // Data Member :: Optional
+  let counter = 1;
+  let id = 100;
+  let pi = 3.14;
+  let str = "Hello World";
+  let active = true;
+
+  // Member function :: Optional
+  let likeMeAction = () => {};
+  let dislikeMeAction = () => {};
+
+  // JSX :: VIEW :: USER-INTERFACE :: Required
+  return (
+    <>
+      <h1>{counter}</h1>
+      <input type="button" value="Like Me" />
     </>
   );
 }
