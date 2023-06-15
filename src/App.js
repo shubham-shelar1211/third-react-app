43167f991b0aa015a008a1224f047832e5cf14ab
Shubham Shelar <shubhamshelar1211@gmail.com>
2023-06-15 11:14:05 +0530

    basic counter.

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 function App() {
   return (
     <>
@@ -9,14 +11,23 @@ function App() {
 
 // UserDefineTag
 function CounterApp() {
+  // Data Member :: stateless/ stateful
+  // let counter = 1; // stateless
+  let [counter, setCounter] = useState(1); // to create stateful variable + this will help to perform dom operation.
+
   // Member Function
   let likeMeAction = () => {
-    // alert();
-    console.log("I m button click");
+    counter = counter + 1;
+    console.log("I m button click", counter);
+
+    // Asking REACT TO PERFOMR DOM OEPRATION
+    // Calling setCounter => DOM Operation
+    setCounter(counter);
   };
 
   return (
     <>
+      <h1>{counter}</h1>
       <input type="button" value="Like Me" onClick={likeMeAction} />
     </>
   );
